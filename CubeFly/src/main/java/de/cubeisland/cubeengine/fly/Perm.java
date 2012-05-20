64302d830eefded4ee728ee277f4cf9d9647d06d
Anselm Brehme <Faithcaio@yahoo.de>
2012-05-20 16:17:10 +0200

    [Fly] Permission

--- CubeFly/src/main/java/de/cubeisland/cubeengine/fly/Perm.java
+++ CubeFly/src/main/java/de/cubeisland/cubeengine/fly/Perm.java
@@ -1,7 +1,7 @@
 package de.cubeisland.cubeengine.fly;
 
 import de.cubeisland.cubeengine.core.CubeCore;
-import de.cubeisland.cubeengine.core.permission.CubePermission;
+import de.cubeisland.cubeengine.core.permission.Permission;
 import org.bukkit.entity.Player;
 import org.bukkit.permissions.PermissionDefault;
 
@@ -9,23 +9,33 @@ import org.bukkit.permissions.PermissionDefault;
  *
  * @author Faithcaio
  */
-public enum Perm
+public enum Perm implements Permission
 {
-    COMMAND_FLY("command.fly",PermissionDefault.OP),
-    COMMAND_FLY_BYPASS("command.fly.bypass",PermissionDefault.OP)
-    ;
-    
+    COMMAND_FLY("command.fly", PermissionDefault.OP),
+    COMMAND_FLY_BYPASS("command.fly.bypass", PermissionDefault.OP);
     private String BASE = "cubeengine.fly.";
-    private CubePermission permission;
-    
-    private Perm(String permission, PermissionDefault def)
+    private String permission;
+    private PermissionDefault permissionDefault;
+
+    private Perm(String permission, PermissionDefault permissionDefault)
     {
-        this.permission = new CubePermission(BASE+permission, def);
-        CubeCore.getInstance().getPermissionRegistration().registerPermission(this.permission);
+        this.permission = BASE + permission;
+        this.permissionDefault = permissionDefault;
+        CubeCore.getInstance().getPermissionRegistration().registerPermission(this);
     }
-    
-    public boolean check(Player player)
+
+    public boolean isAuthorized(Player player)
     {
-        return permission.isAuthorized(player);
+        return player.hasPermission(permission);
+    }
+
+    public String getPermission()
+    {
+        return this.permission;
+    }
+
+    public PermissionDefault getPermissionDefault()
+    {
+        return this.permissionDefault;
     }
 }
