f28a841a80047698315f34dab43caf2e6a9b8816
Anselm Brehme <Faithcaio@yahoo.de>
2012-05-20 22:55:45 +0200

    [Core] Unit Testing

---
+++ CubeCore/src/test/java/de/cubeisland/cubeengine/core/TestTest.java
@@ -0,0 +1,13 @@
+package de.cubeisland.cubeengine.core;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class TestTest extends TestCase
+{
+    @Test
+    public void testNothing()
+    {
+        
+    }
+}
