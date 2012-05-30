c904335e22c71112ed5ed2f3afd7600bb2ec8b70
Anselm Brehme <Faithcaio@yahoo.de>
2012-05-30 10:56:12 +0200

    [Core] save config

--- CubeCore/src/main/java/de/cubeisland/cubeengine/core/persistence/filesystem/ModuleConfiguration.java
+++ CubeCore/src/main/java/de/cubeisland/cubeengine/core/persistence/filesystem/ModuleConfiguration.java
@@ -1,7 +1,8 @@
 package de.cubeisland.cubeengine.core.persistence.filesystem;
 
+import de.cubeisland.cubeengine.core.module.Module;
 import java.lang.reflect.Field;
-import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.configuration.file.FileConfiguration;
 
 /**
  *
@@ -9,19 +10,22 @@ import org.bukkit.configuration.file.YamlConfiguration;
  */
 public class ModuleConfiguration
 {
-    protected final YamlConfiguration config;
+    protected final FileConfiguration config;
+    private final Module module;
 
-    public ModuleConfiguration()
+    public ModuleConfiguration(Module module)
     {
-        config = new YamlConfiguration();
+        this.module = module;
+        config = module.getConfig();
     }
 
-    private void loadConfiguration(Class<? extends ModuleConfiguration> moduleConfig)
+    public void loadConfiguration(Class<? extends ModuleConfiguration> moduleConfig)
     {
         for (Field field : moduleConfig.getFields())
         {
             this.loadConfigElement(field);
         }
+        module.saveConfig();
     }
 
     private void loadConfigElement(Field field)
@@ -37,7 +41,32 @@ public class ModuleConfiguration
             Object configElem = config.get(path);
             field.set(this, configElem);
         }
-        catch (IllegalAccessException e)
+        catch (IllegalAccessException ex)
+        {
+        }
+    }
+
+    public void saveConfiguration(Class<? extends ModuleConfiguration> moduleConfig)
+    {
+        for (Field field : moduleConfig.getFields())
+        {
+            this.saveConfigElement(field);
+        }
+        module.saveConfig();
+    }
+
+    private void saveConfigElement(Field field)
+    {
+        try
+        {
+            if (!field.isAnnotationPresent(Option.class))
+            {
+                return;
+            }
+            String path = field.getAnnotation(Option.class).value();
+            config.set(path, field.get(this));
+        }
+        catch (IllegalAccessException ex)
         {
         }
     }
