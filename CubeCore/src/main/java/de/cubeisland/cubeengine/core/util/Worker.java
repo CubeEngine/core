eafe8d550383d03a32800ff7659c11847e1eb2c5
Robin Bechtel-Ostmann <rbechtel.ostmann@googlemail.com>
2012-06-07 21:49:34 +0200

    fixed worker (logic)

--- CubeCore/src/main/java/de/cubeisland/cubeengine/core/util/Worker.java
+++ CubeCore/src/main/java/de/cubeisland/cubeengine/core/util/Worker.java
@@ -9,46 +9,64 @@ import java.util.concurrent.ConcurrentLinkedQueue;
  * @author Robin Bechtel-Ostmann
  */
 
-public class Worker
+public class Worker implements Runnable
 {
     private ConcurrentLinkedQueue<Runnable> jobs;
-    private boolean pause;
     private boolean running;
+    private boolean paused;
+    private Thread runner;
     
     public void worker()
     {
         this.jobs = new  ConcurrentLinkedQueue<Runnable>();
-        this.pause = false;
         this.running = false;
+        this.paused = false;
+        this.runner = new Thread(this);
     }
     
-    public synchronized void shedule()
+    @Override
+    public synchronized void run()
     {
         this.running = true;
-        while(!(this.jobs.isEmpty() && pause))
+        while(this.jobs.isEmpty())
         {
-            new Thread(this.jobs.poll()).start();
+            this.jobs.poll().run();
         }
         this.running = false;
     }
     
-    public synchronized void addJob(Runnable job)
+    public synchronized void addJob(Runnable job, boolean autoStart)
     {
         this.jobs.add(job);
-        if(!running)
+        if(!running && autoStart)
         {
-            this.shedule();
+            this.runner.start();
         }
     }
     
-    public void pauseWorker()
+    public synchronized boolean pause()
     {
-        this.pause = true;
+        try
+        {
+            this.runner.wait();
+            this.paused = true;
+            return true;
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace(System.err);
+            return false;
+        }
     }
     
-    public void resume()
+    public synchronized void resume()
     {
-        this.pause = false;
-        this.shedule();
+        this.runner.notify();
+        this.paused = false;
+    }
+    
+    public void dropJobs()
+    {
+        this.jobs.clear();
     }
 }
