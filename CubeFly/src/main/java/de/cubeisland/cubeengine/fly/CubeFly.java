07613f473d5d2ae8f887ede596c26da7d258c5b4
Phillip Schichtel <quick_wango@code-infection.de>
2012-06-26 23:19:48 +0200

    cleaned up CubeFly

--- CubeFly/src/main/java/de/cubeisland/cubeengine/fly/CubeFly.java
+++ CubeFly/src/main/java/de/cubeisland/cubeengine/fly/CubeFly.java
@@ -1,18 +1,16 @@
 package de.cubeisland.cubeengine.fly;
 
+import de.cubeisland.cubeengine.CubeEngine;
 import de.cubeisland.cubeengine.core.CubeCore;
 import de.cubeisland.cubeengine.core.command.BaseCommand;
 import de.cubeisland.cubeengine.core.module.ModuleBase;
 import de.cubeisland.cubeengine.core.persistence.filesystem.config.Configuration;
 import java.io.File;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.bukkit.Server;
 import org.bukkit.plugin.PluginManager;
 
 public class CubeFly extends ModuleBase
 {
-    protected static Logger logger = null;
     public static boolean debugMode = false;
     protected Server server;
     protected PluginManager pm;
@@ -29,16 +27,11 @@ public class CubeFly extends ModuleBase
     @Override
     public void onEnable()
     {
-        logger = this.getLogger();
-        this.server = this.getServer();
-        this.pm = this.server.getPluginManager();
-        this.dataFolder = this.getDataFolder();
-
-        this.dataFolder.mkdirs();
+        CubeEngine.registerModule(this);
+        
+        this.pm = this.getServer().getPluginManager();
 
         this.config = Configuration.load(this, FlyConfiguration.class);
-
-        debugMode = this.config.debugMode;
 
         this.baseCommand = new BaseCommand(this, PERMISSION_BASE);
         this.baseCommand.registerCommands(new FlyCommand()).setDefaultCommand("fly").unregisterCommand("reload");
@@ -46,37 +39,6 @@ public class CubeFly extends ModuleBase
 
         this.pm.registerEvents(new FlyListener(CubeCore.getInstance().getUserManager(), this), this);
 
-        this.getCore().registerPermissions(Perm.values());
-
-        log("Version " + this.getDescription().getVersion() + " enabled");
-    }
-
-    @Override
-    public void onDisable()
-    {
-        log("Version " + this.getDescription().getVersion() + " disabled");
-    }
-
-    public static void log(String msg)
-    {
-        logger.log(Level.INFO, msg);
-    }
-
-    public static void error(String msg)
-    {
-        logger.log(Level.SEVERE, msg);
-    }
-
-    public static void error(String msg, Throwable t)
-    {
-        logger.log(Level.SEVERE, msg, t);
-    }
-
-    public static void debug(String msg)
-    {
-        if (debugMode)
-        {
-            log("[debug] " + msg);
-        }
+        CubeEngine.registerPermissions(Perm.values());
     }
 }
