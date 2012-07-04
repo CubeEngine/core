ab62bfb887f4e001b382b8f5262f7ff5a089d387
Robin Bechtel-Ostmann <rbechtel.ostmann@googlemail.com>
2012-07-04 02:06:21 +0200

    resolved using ArrayList instead of a worker-array

--- CubeCore/src/main/java/de/cubeisland/cubeengine/core/util/worker/WorkerPool.java
+++ CubeCore/src/main/java/de/cubeisland/cubeengine/core/util/worker/WorkerPool.java
@@ -1,5 +1,7 @@
 package de.cubeisland.cubeengine.core.util.worker;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Robin Bechtel-Ostmann
@@ -7,29 +9,48 @@ package de.cubeisland.cubeengine.core.util.worker;
 
 public class WorkerPool
 {
-    private Worker[] workers;
-    private int workerCount;
+    private ArrayList<Worker> workers;
+    private int maxWorkerCount;
     
-    public WorkerPool(int workerCount)
+    public WorkerPool(int maxWorkerCount)
     {
-        this.workerCount = workerCount;
-        this.workers = new Worker[this.workerCount];
-        
-        for(int i = 0; i < this.workerCount; i++)
-        {
-            this.workers[i] = new Worker();
-        }
+        this.maxWorkerCount = maxWorkerCount;
+        this.workers = new ArrayList();
     }
     
     public void addJob(Runnable job)
     {
-        int smallest = this.workers[0].getJobCount();
+        boolean jobDelivered = false;
+        
+        for(Worker worker : this.workers)
+        {
+            if(worker.getJobCount() == 0 && !jobDelivered)
+            {
+                worker.addJob(job, true);
+                jobDelivered = true;
+            }
+            else if(worker.getJobCount() == 0 && jobDelivered)
+            {
+                this.workers.remove(worker);
+            }
+        }
+        
+        int workersCount = this.workers.size();
+        
+        if(!jobDelivered && workersCount < this.maxWorkerCount)
+        {
+            this.workers.add(new Worker());
+            this.workers.get(workersCount).addJob(job, true);
+            jobDelivered = true;
+        }
+
+        int smallest = this.workers.get(0).getJobCount();
         int smallestIndex = 0;
         int next;
         
-        for(int i = 1; i < this.workerCount; i++)
+        for(int i = 1; i < workersCount || !jobDelivered; i++)
         {
-            next = this.workers[i].getJobCount();
+            next = this.workers.get(i).getJobCount();
             if(smallest > next)
             {
                 smallest = next;
@@ -37,36 +58,36 @@ public class WorkerPool
             }
         }
         
-        this.workers[smallestIndex].addJob(job, true);
+        this.workers.get(smallestIndex).addJob(job, true);
     }
     
     public void resume()
     {
-        for(int i = 1; i < this.workerCount; i++)
+        for(Worker worker : this.workers)
         {
-            this.workers[i].resume();
+            worker.resume();
         }
     }
     
     public void pause()
     {
-        for(int i = 1; i < this.workerCount; i++)
+        for(Worker worker : this.workers)
         {
-            this.workers[i].pause();
+            worker.pause();
         }
     }
     
     //kills all jobs running in all workers
     public void stop()
     {
-        for(int i = 1; i < this.workerCount; i++)
+        for(Worker worker : this.workers)
         {
-            this.workers[i].stop();
+            worker.stop();
         }
     }
     
     public int getWorkerCount()
     {
-        return this.workerCount;
+        return this.workers.size();
     }
 }
