3865458eb52747b944e52e62c9254ffcacc5854f
Stefan Wolf <boeserwolf91@googlemail.com>
2012-09-13 21:58:18 +0200

    The mainclass of this plugin

---
+++ CubeRuleBook/src/main/java/de/cubeisland/cubeengine/rulebook/Rulebook.java
@@ -0,0 +1,43 @@
+package de.cubeisland.cubeengine.rulebook;
+
+import de.cubeisland.cubeengine.core.CubeEngine;
+import de.cubeisland.cubeengine.core.module.Module;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ * Main Class
+ */
+public class Rulebook extends Module
+{
+    private RuleBookConfiguration config;
+    private Logger logger;
+    
+    @Override
+    public void onEnable()
+    {
+        this.logger = this.getLogger();
+        CubeEngine.getFileManager().dropResources(RuleBookResource.values());
+        
+        this.config = new RuleBookConfiguration(this);
+        this.getCore().getEventManager().registerListener(new NewPlayerJoinListener(this), this); 
+    }
+    
+    @Override
+    public void onDisable()
+    {
+        logger = null;
+    }
+    
+    public void error(String msg, Throwable t)
+    {
+        logger.log(Level.SEVERE, msg, t);
+    }
+    
+    public RuleBookConfiguration getConfig()
+    {
+        return this.config;
+    }
+}
+
