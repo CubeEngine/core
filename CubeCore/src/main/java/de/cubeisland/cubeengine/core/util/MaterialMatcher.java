00baabf4603980a89a96919abb7a621f9bfb80ab
Anselm Brehme <Faithcaio@yahoo.de>
2012-09-16 03:40:44 +0200

    #26 special cases for Materials (maybe make a Entity Matcher too)

--- CubeCore/src/main/java/de/cubeisland/cubeengine/core/util/MaterialMatcher.java
+++ CubeCore/src/main/java/de/cubeisland/cubeengine/core/util/MaterialMatcher.java
@@ -117,15 +117,15 @@ public class MaterialMatcher
                     {
                         item.setDurability((short)0);
                     }
-                    if (data.equals("pine")||data.equals("spruce"))
+                    else if (data.equals("pine") || data.equals("spruce"))
                     {
                         item.setDurability((short)1);
                     }
-                    if (data.equals("birch"))
+                    else if (data.equals("birch"))
                     {
                         item.setDurability((short)2);
                     }
-                    if (data.equals("jungle"))
+                    else if (data.equals("jungle"))
                     {
                         item.setDurability((short)3);
                     }
@@ -134,29 +134,386 @@ public class MaterialMatcher
                     if (data.equals("white"))
                     {
                         item.setDurability((short)0);
-                    }//TODO more ...
-                    
+                    }
+                    else if (data.equals("orange"))
+                    {
+                        item.setDurability((short)1);
+                    }
+                    else if (data.equals("magenta"))
+                    {
+                        item.setDurability((short)2);
+                    }
+                    else if (data.equals("lightblue") || data.equals("lblue"))
+                    {
+                        item.setDurability((short)3);
+                    }
+                    else if (data.equals("yellow"))
+                    {
+                        item.setDurability((short)4);
+                    }
+                    else if (data.equals("lime") || data.equals("lgreen"))
+                    {
+                        item.setDurability((short)5);
+                    }
+                    else if (data.equals("pink"))
+                    {
+                        item.setDurability((short)6);
+                    }
+                    else if (data.equals("gray"))
+                    {
+                        item.setDurability((short)7);
+                    }
+                    else if (data.equals("lightgray") || data.equals("lgray"))
+                    {
+                        item.setDurability((short)8);
+                    }
+                    else if (data.equals("cyan"))
+                    {
+                        item.setDurability((short)9);
+                    }
+                    else if (data.equals("purple"))
+                    {
+                        item.setDurability((short)10);
+                    }
+                    else if (data.equals("blue"))
+                    {
+                        item.setDurability((short)11);
+                    }
+                    else if (data.equals("brown"))
+                    {
+                        item.setDurability((short)12);
+                    }
+                    else if (data.equals("green"))
+                    {
+                        item.setDurability((short)13);
+                    }
+                    else if (data.equals("red"))
+                    {
+                        item.setDurability((short)14);
+                    }
+                    else if (data.equals("black"))
+                    {
+                        item.setDurability((short)15);
+                    }
                     return item;
                 case INK_SACK:
-                    // TODO colors
+                    if (data.equals("white"))
+                    {
+                        item.setDurability((short)15);
+                    }
+                    else if (data.equals("orange"))
+                    {
+                        item.setDurability((short)14);
+                    }
+                    else if (data.equals("magenta"))
+                    {
+                        item.setDurability((short)13);
+                    }
+                    else if (data.equals("lightblue") || data.equals("lblue"))
+                    {
+                        item.setDurability((short)12);
+                    }
+                    else if (data.equals("yellow"))
+                    {
+                        item.setDurability((short)11);
+                    }
+                    else if (data.equals("lime") || data.equals("lgreen"))
+                    {
+                        item.setDurability((short)10);
+                    }
+                    else if (data.equals("pink"))
+                    {
+                        item.setDurability((short)9);
+                    }
+                    else if (data.equals("gray"))
+                    {
+                        item.setDurability((short)8);
+                    }
+                    else if (data.equals("lightgray") || data.equals("lgray"))
+                    {
+                        item.setDurability((short)7);
+                    }
+                    else if (data.equals("cyan"))
+                    {
+                        item.setDurability((short)6);
+                    }
+                    else if (data.equals("purple"))
+                    {
+                        item.setDurability((short)5);
+                    }
+                    else if (data.equals("blue"))
+                    {
+                        item.setDurability((short)4);
+                    }
+                    else if (data.equals("brown"))
+                    {
+                        item.setDurability((short)3);
+                    }
+                    else if (data.equals("green"))
+                    {
+                        item.setDurability((short)2);
+                    }
+                    else if (data.equals("red"))
+                    {
+                        item.setDurability((short)1);
+                    }
+                    else if (data.equals("black"))
+                    {
+                        item.setDurability((short)0);
+                    }
                     return item;
                 case DOUBLE_STEP:
                 case STEP:
-                    //TODO stoneslabs
+                    if (data.equals("clean") || data.equals("stone"))
+                    {
+                        item.setDurability((short)0);
+                    }
+                    else if (data.equals("sandstone"))
+                    {
+                        item.setDurability((short)1);
+                    }
+                    else if (data.equals("wood"))
+                    {
+                        item.setDurability((short)2);
+                    }
+                    else if (data.equals("cobble"))
+                    {
+                        item.setDurability((short)3);
+                    }
+                    else if (data.equals("brick"))
+                    {
+                        item.setDurability((short)4);
+                    }
+                    else if (data.equals("stonebrick"))
+                    {
+                        item.setDurability((short)5);
+                    }
                     return item;
                 case SANDSTONE:
-                    //TODO sandstone
-                    return item;
-                case JUKEBOX: // TODO check if even possible
+                    if (data.equals("normal"))
+                    {
+                        item.setDurability((short)0);
+                    }
+                    else if (data.equals("chiseled") || data.equals("creeper"))
+                    {
+                        item.setDurability((short)1);
+                    }
+                    else if (data.equals("smooth") || data.equals("clean"))
+                    {
+                        item.setDurability((short)2);
+                    }
                     return item;
                 case BRICK:
+                    if (data.equals("normal"))
+                    {
+                        item.setDurability((short)0);
+                    }
+                    else if (data.equals("mossy"))
+                    {
+                        item.setDurability((short)1);
+                    }
+                    else if (data.equals("cracked") || data.equals("old"))
+                    {
+                        item.setDurability((short)2);
+                    }
+                    else if (data.equals("chiseled") || data.equals("round"))
+                    {
+                        item.setDurability((short)3);
+                    }
                     return item;
                 case HUGE_MUSHROOM_1:
                 case HUGE_MUSHROOM_2:
+                    if (data.equals("pores"))
+                    {
+                        item.setDurability((short)0);
+                    }
+                    else if (data.equals("cap"))
+                    {
+                        item.setDurability((short)5);
+                    }
+                    if (data.equals("stem"))
+                    {
+                        item.setDurability((short)10);
+                    }
+                    else if (data.equals("allstem"))
+                    {
+                        item.setDurability((short)14);
+                    }
+                    else if (data.equals("allcap"))
+                    {
+                        item.setDurability((short)15);
+                    }
                     return item;
                 case POTION:
+                    if (data.equals("water"))
+                    {
+                        item.setDurability((short)0);
+                    }
+                    else if (data.equals("awkward"))
+                    {
+                        item.setDurability((short)16);
+                    }
+                    else if (data.equals("thick"))
+                    {
+                        item.setDurability((short)32);
+                    }
+                    else if (data.equals("mundane"))
+                    {
+                        item.setDurability((short)64);
+                    }
+                    else
+                    {
+                        short dv = 0;
+                        // Effect Type:
+                        if (data.contains("regen"))
+                        {
+                            dv += 1;
+                        }
+                        else if (data.contains("speed"))
+                        {
+                            dv += 2;
+                        }
+                        else if (data.contains("fireresistance"))
+                        {
+                            dv += 3;
+                        }
+                        else if (data.contains("poison"))
+                        {
+                            dv += 4;
+                        }
+                        else if (data.contains("heal"))
+                        {
+                            dv += 5;
+                        }
+                        else if (data.contains("nightvision"))
+                        {
+                            dv += 6;
+                        }
+                        else if (data.contains("weak"))
+                        {
+                            dv += 8;
+                        }
+                        else if (data.contains("strength"))
+                        {
+                            dv += 9;
+                        }
+                        else if (data.contains("slow"))
+                        {
+                            dv += 10;
+                        }
+                        else if (data.contains("damage"))
+                        {
+                            dv += 12;
+                        }
+                        else if (data.contains("invisible") || data.contains("invisibility"))
+                        {
+                            dv += 14;
+                        }
+                        // Tier
+                        if (data.contains("2"))
+                        {
+                            dv += 32;
+                        }
+                        // Duration
+                        if (data.contains("long"))
+                        {
+                            dv += 32;
+                        }
+                        // splash
+                        if (data.contains("splash"))
+                        {
+                            dv += 16384;
+                        }
+                        item.setDurability(dv);
+                    }
                     return item;
-                case MOB_SPAWNER://TODO can do this probably not
+                case MONSTER_EGG: // TODO Entity Matcher ??
+                    if (data.equals("creeper"))
+                    {
+                        item.setDurability((short)50);
+                    }
+                    else if (data.equals("skeleton"))
+                    {
+                        item.setDurability((short)51);
+                    }
+                    else if (data.equals("spider"))
+                    {
+                        item.setDurability((short)52);
+                    }
+                    else if (data.equals("zombie"))
+                    {
+                        item.setDurability((short)54);
+                    }
+                    else if (data.equals("slime"))
+                    {
+                        item.setDurability((short)55);
+                    }
+                    else if (data.equals("ghast"))
+                    {
+                        item.setDurability((short)56);
+                    }
+                    else if (data.equals("zombiepigman"))
+                    {
+                        item.setDurability((short)57);
+                    }
+                    else if (data.equals("enderman"))
+                    {
+                        item.setDurability((short)58);
+                    }
+                    else if (data.equals("cavespider"))
+                    {
+                        item.setDurability((short)59);
+                    }
+                    else if (data.equals("silverfish"))
+                    {
+                        item.setDurability((short)60);
+                    }
+                    else if (data.equals("blaze"))
+                    {
+                        item.setDurability((short)61);
+                    }
+                    else if (data.equals("magmacube"))
+                    {
+                        item.setDurability((short)62);
+                    }
+                    else if (data.equals("pig"))
+                    {
+                        item.setDurability((short)90);
+                    }
+                    else if (data.equals("sheep"))
+                    {
+                        item.setDurability((short)91);
+                    }
+                    else if (data.equals("cow"))
+                    {
+                        item.setDurability((short)92);
+                    }
+                    else if (data.equals("chicken"))
+                    {
+                        item.setDurability((short)93);
+                    }
+                    else if (data.equals("squid"))
+                    {
+                        item.setDurability((short)94);
+                    }
+                    else if (data.equals("wolf"))
+                    {
+                        item.setDurability((short)95);
+                    }
+                    else if (data.equals("mooshroom"))
+                    {
+                        item.setDurability((short)96);
+                    }
+                    else if (data.equals("ocelot"))
+                    {
+                        item.setDurability((short)98);
+                    }
+                    else if (data.equals("villager"))
+                    {
+                        item.setDurability((short)120);
+                    }
+                    return item;
+                case JUKEBOX: // TODO check if even possible
                     return item;
                 default:
                     return null;
