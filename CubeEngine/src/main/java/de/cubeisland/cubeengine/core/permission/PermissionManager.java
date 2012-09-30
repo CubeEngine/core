ce89a712d08523e312f601eb458aee513b7d5792
Phillip Schichtel <quick_wango@code-infection.de>
2012-09-30 02:07:48 +0200

    experimantal permission registration improvement
    the idea is to dynamicly register a wildcard permission for all nodes and update them when new child nodes get registered to easy and speed-up permission assignment

--- CubeEngine/src/main/java/de/cubeisland/cubeengine/core/permission/PermissionManager.java
+++ CubeEngine/src/main/java/de/cubeisland/cubeengine/core/permission/PermissionManager.java
@@ -1,9 +1,11 @@
 package de.cubeisland.cubeengine.core.permission;
 
 import de.cubeisland.cubeengine.core.module.Module;
+import de.cubeisland.cubeengine.core.util.StringUtils;
 import gnu.trove.map.hash.THashMap;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import org.apache.commons.lang.Validate;
 import org.bukkit.permissions.PermissionDefault;
@@ -16,17 +18,35 @@ import org.bukkit.plugin.PluginManager;
  */
 public class PermissionManager
 {
+    private static final String CUBEENGINE_WILDCARD = "cubeengine.*";
+    
     private final PluginManager pm;
-    private final Map<String, Permission> wildcardPermission;
+    private final Map<String, org.bukkit.permissions.Permission> wildcardPermission;
     private final Map<Module, List<String>> modulePermissionMap;
     private final Thread mainThread;
 
     public PermissionManager(PluginManager pm)
     {
         this.pm = pm;
-        this.wildcardPermission = new THashMap<String, Permission>(0);
+        this.wildcardPermission = new THashMap<String, org.bukkit.permissions.Permission>(0);
         this.modulePermissionMap = new THashMap<Module, List<String>>(0);
         this.mainThread = Thread.currentThread();
+        
+        this.registerBukkitPermission(new org.bukkit.permissions.Permission("cubeengine.*"));
+    }
+    
+    private void registerBukkitPermission(org.bukkit.permissions.Permission permission)
+    {
+        try
+        {
+            this.pm.addPermission(permission);
+            if (permission.getName().endsWith("*"))
+            {
+                this.wildcardPermission.put(permission.getName(), permission);
+            }
+        }
+        catch (IllegalArgumentException ignored)
+        {}
     }
 
     /**
@@ -45,18 +65,45 @@ public class PermissionManager
         Validate.notNull(module, "The module must not be null!");
         Validate.notNull(perm, "The permission must not be null!");
         Validate.notNull(permDefault, "The permission default must not be null!");
-        try
+        
+        if (perm.equals(CUBEENGINE_WILDCARD))
         {
-            List<String> modulePermissions = this.modulePermissionMap.get(module);
-            if (modulePermissions == null)
-            {
-                this.modulePermissionMap.put(module, modulePermissions = new ArrayList<String>(1));
-            }
-            modulePermissions.add(perm);
-            this.pm.addPermission(new org.bukkit.permissions.Permission(perm, permDefault));
+            return this;
         }
-        catch (IllegalArgumentException ignored)
-        {}
+
+        perm = perm.toLowerCase(Locale.ENGLISH);
+        String[] parts = StringUtils.explode(".", perm);
+        if (parts.length < 3 || !"cubeengine".equals(parts[0]) || !module.getId().equals(parts[1]))
+        {
+            throw new IllegalArgumentException("Permissions must start with 'cubeengine.<module>' !" );
+        }
+        
+        List<String> modulePermissions = this.modulePermissionMap.get(module);
+        if (modulePermissions == null)
+        {
+            this.modulePermissionMap.put(module, modulePermissions = new ArrayList<String>(1));
+        }
+        modulePermissions.add(perm);
+        org.bukkit.permissions.Permission permission = new org.bukkit.permissions.Permission(perm, permDefault);
+        this.registerBukkitPermission(permission);
+        
+        org.bukkit.permissions.Permission oldPermission = permission;
+        String base = "cubeengine.module.";
+        
+        permission = this.wildcardPermission.get(base + "*");
+        if (permission == null)
+        {
+            permission = new org.bukkit.permissions.Permission(base + "*", PermissionDefault.FALSE);
+        }
+        this.registerBukkitPermission(permission);
+        oldPermission.addParent(permission, true);
+        
+        for (int i = 2; i < parts.length; ++i)
+        {
+            
+        }
+        
+        permission.addParent(permission, true);
         
         return this;
     }
@@ -91,11 +138,16 @@ public class PermissionManager
     {
         Validate.notNull(module, "The module must not be null!");
         Validate.notNull(perm, "The permission must not be null!");
+        Validate.isTrue(!perm.equals(CUBEENGINE_WILDCARD), "The CubeEngine wildcard permission must not be unregistered!");
         
         List<String> perms = this.modulePermissionMap.get(module);
         if (perms != null && perms.remove(perm))
         {
             this.pm.removePermission(perm);
+            if (perm.endsWith("*"))
+            {
+                this.wildcardPermission.remove(perm);
+            }
         }
         
         return this;
@@ -110,6 +162,10 @@ public class PermissionManager
             for (String perm : removedPerms)
             {
                 this.pm.removePermission(perm);
+                if (perm.endsWith("*"))
+                {
+                    this.wildcardPermission.remove(perm);
+                }
             }
         }
         
