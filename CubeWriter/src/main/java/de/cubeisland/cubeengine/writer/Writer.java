3266069ad6f8e4f0387791978daf1c15b11528e2
totokaka <ttotokaka@gmail.com>
2012-10-09 21:14:37 +0200

    Setting up basics of CubeWriter
    Based on CubeTest

---
+++ CubeWriter/src/main/java/de/cubeisland/cubeengine/writer/Writer.java
@@ -0,0 +1,21 @@
+package de.cubeisland.cubeengine.writer;
+
+import de.cubeisland.cubeengine.core.module.Module;
+
+
+public class Writer extends Module
+{
+    @Override
+    public void onEnable()
+    {
+
+    }
+
+
+    @Override
+    public void onDisable()
+    {
+    }
+
+
+}
