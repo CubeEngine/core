09707adb6d23755409194b90cbb52bd4e3f42a0c
totokaka <ttotokaka@gmail.com>
2012-10-12 20:15:03 +0200

    [Writer] The edit command is almost done

--- CubeWriter/src/main/java/de/cubeisland/cubeengine/writer/EditCommand.java
+++ CubeWriter/src/main/java/de/cubeisland/cubeengine/writer/EditCommand.java
@@ -1,9 +1,12 @@
 package de.cubeisland.cubeengine.writer;
 
+import java.util.Map;
+
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
+import org.bukkit.inventory.ItemStack;
 
 import de.cubeisland.cubeengine.core.command.CommandContext;
 import de.cubeisland.cubeengine.core.command.annotation.Command;
@@ -16,74 +19,73 @@ public class EditCommand
 	@Command(
 			names = {"edit", "rewrite"}, 
 			desc = "Edit a sign or a book",
-			min = 1,
+			min = 0,
 			max = 4,
 			usage = "Look at a sign and excecute command. Example:\n /edit Line1 This is line one 2 \"this line contains a number 2\" 3 This is line three",
 			params = {
 					@Param(
-							names = {"Line1", "1"}, 
+							names = {"1", "Line1"}, 
 							types = {String.class}
 						),
 					@Param(
-							names = {"Line2", "2"}, 
+							names = {"2", "Line2"}, 
 							types = {String.class}
 						),
 					@Param(
-							names = {"Line3", "3"}, 
+							names = {"3", "Line3"}, 
 							types = {String.class}
 						),
 					@Param(
-							names = {"Line3", "3"}, 
+							names = {"4", "Line4"}, 
 							types = {String.class}
-						),
+						)
 					}
 		)
 	public void edit(CommandContext context)
 	{
-		// TODO add books
-		User user = context.getSenderAsUser();
 		
-		if (user == null)
+		User user = context.getSenderAsUser("writer", "This command can only be used from ingame");
+		
+		if (user.getItemInHand().getType() == Material.WRITTEN_BOOK)
 		{
-			context.getSender().sendMessage("This commands can only be used from ingame");
-			return;
+			ItemStack item = user.getItemInHand();
+			BookItem unsigned = new BookItem(item);
+			unsigned.setAuthor("");
+			unsigned.setTitle("");
+			item = unsigned.getItemStack();
+			item.setType(Material.BOOK_AND_QUILL);
+			user.sendMessage("Your book is now unsigned and ready to be edited");
+			
+		}
+		else
+		{
+			Block target = user.getTargetBlock(null, 10);
+			
+			if (!(target.getType() == Material.SIGN || target.getType() == Material.SIGN_POST))
+			{
+				user.sendMessage(ChatColor.RED + "You need to be looking at a sign less than 10 blocks away");
+				return;
+			}
+			
+			if (context.namedCount() < 0){
+				user.sendMessage(ChatColor.RED + "You need to speccify at least one parameter");
+				return;
+			}
+			
+			Sign sign = (Sign)target.getState();
+			
+			Map<String, Object[]> params = context.getNamed();
+			for (String key : params.keySet())
+			{
+				sign.setLine(Integer.parseInt(key) - 1, context.getNamed(key, String.class));
+			}
+			
+			// TODO update the block to the sign..
+			
+			user.sendMessage("The sign has been changed");	
 		}
 		
-		Block target = user.getTargetBlock(null, 10);
 		
-		if (!(target.getType() == Material.SIGN || target.getType() == Material.SIGN_POST))
-		{
-			user.sendMessage(ChatColor.RED + "You need to be looking at a sign less than 10 blocks away");
-			return;
-		}
-		
-		if (context.namedCount() < 0){
-			user.sendMessage(ChatColor.RED + "You need to speccify at least one parameter");
-			return;
-		}
-		
-		Sign sign = (Sign)target;
-		
-		if (context.hasNamed("Line1"))
-		{
-			user.sendMessage("Line one is: " + context.getNamed("Line1", String.class));
-			sign.setLine(1, context.getNamed("Line1", String.class));
-		}
-		if (context.hasNamed("Line2"))
-		{
-			user.sendMessage("Line two is: " + context.getNamed("Line2", String.class));
-			sign.setLine(2, context.getNamed("Line2", String.class));
-		}
-		if (context.hasNamed("Line3"))
-		{
-			user.sendMessage("Line three is: " + context.getNamed("Line3", String.class));
-			sign.setLine(3, context.getNamed("Line3", String.class));
-		}
-		if (context.hasNamed("Line4"))
-		{
-			user.sendMessage("Line four is: " + context.getNamed("Line4", String.class));
-			sign.setLine(4, context.getNamed("Line4", String.class));
-		}
 	}
 	
 }
