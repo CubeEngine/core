260be0a780f93c7e6de7e05d3e9ab8602e80d92a
Phillip Schichtel <quick_wango@code-infection.de>
2012-10-14 18:21:14 +0200

    only if not too many commands match the string

--- CubeEngine/src/main/java/de/cubeisland/cubeengine/core/bukkit/CubeCommandMap.java
+++ CubeEngine/src/main/java/de/cubeisland/cubeengine/core/bukkit/CubeCommandMap.java
@@ -96,7 +96,7 @@ public class CubeCommandMap extends SimpleCommandMap
                 command = this.getCommand(label);
                 
             }
-            else if (matches.size() > 1)
+            else if (matches.size() > 1 && matches.size() <= 5) // TODO maximum configurable
             {
                 sender.sendMessage(_(sender, "core", "I could not find the command /%s ...", label));
                 sender.sendMessage(_(sender, "core", "Did you mean once of these: %s ?", "/" + StringUtils.implode(", /", matches)));
