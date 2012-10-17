90ce93817ea897a0840d476f78d45e4d584d5372
totokaka <ttotokaka@gmail.com>
2012-10-17 16:21:51 +0200

    [Shout] Working on command

--- CubeShout/src/main/java/de/cubeisland/cubeengine/shout/interactions/ShoutCommand.java
+++ CubeShout/src/main/java/de/cubeisland/cubeengine/shout/interactions/ShoutCommand.java
@@ -1,5 +1,11 @@
 package de.cubeisland.cubeengine.shout.interactions;
 
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+import de.cubeisland.cubeengine.core.command.CommandContext;
+import de.cubeisland.cubeengine.core.command.annotation.Command;
+import de.cubeisland.cubeengine.core.command.annotation.Param;
 import de.cubeisland.cubeengine.shout.Shout;
 
 public class ShoutCommand{
@@ -10,4 +16,47 @@ public class ShoutCommand{
 		this.module = module;;
 	}
 	
+    @Command(
+    	names = {"broadcast", "say", "shout", "announce"},
+    	min = 1,
+    	desc = "Announce a message to players on the server",
+    	usage = "<[World world] <message> | <Announcment-name> >",
+    	params =
+    	    {
+    	        @Param(names = {"World", "W"},types = {String.class})
+    	    }
+    )
+	public void broadcast(CommandContext context)
+	{
+		if (context.hasNamed("World") || context.indexedCount() > 1)
+		{
+			String[] message = (String[])context.getIndexed().toArray();
+			World world = module.getCore().getServer().getWorld(context.getNamed("World", String.class));
+			if (world != null)
+			{
+				for (Player p : world.getPlayers())
+				{
+					p.sendMessage(message);
+				}
+			}
+			else
+			{
+				module.getCore().getServer().broadcastMessage(arrayToString(message));
+			}
+		}
+		else if (context.indexedCount() == 1)
+		{
+			// TODO
+		}
+	}
+	
+    private String arrayToString(String[] array)
+    {
+    	StringBuilder result = new StringBuilder();
+    	for (String s : array)
+    	{
+    		result.append(s);
+    	}
+    	return result.toString().trim();
+    }
 }
