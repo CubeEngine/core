11827f6c7661f94a4061ea75b26389ffa0a5acef
totokaka <ttotokaka@gmail.com>
2012-11-07 20:30:09 +0100

    [Shout] final formatting fix

--- CubeShout/src/test/java/de/cubeisland/cubeengine/shout/task/AnnouncementManagerTest.java
+++ CubeShout/src/test/java/de/cubeisland/cubeengine/shout/task/AnnouncementManagerTest.java
@@ -5,23 +5,22 @@ import static org.junit.Assert.assertEquals;
 import de.cubeisland.cubeengine.shout.Shout;
 import org.junit.Test;
 
-public class AnnouncementManagerTest {
-	// TODO test the actual class, not single methods
-	
-	@Test
-	public void testParseDelay()
-	{
-		AnnouncementManager testamanager = new AnnouncementManager(new Shout());
-		
-		assertEquals(1000l, testamanager.parseDelay("1 secound"));
-		assertEquals(60000l, testamanager.parseDelay("1 minute"));
-		assertEquals(3600000l, testamanager.parseDelay("1 hour"));
-		assertEquals(86400000l, testamanager.parseDelay("1 day"));
-		
-		assertEquals(172800000l, testamanager.parseDelay("2 days"));
-		assertEquals(2000l, testamanager.parseDelay("2 secounds"));
-		assertEquals(120000l, testamanager.parseDelay("2 minutes"));
-		assertEquals(7200000l, testamanager.parseDelay("2 hours"));
-	}
-	
+public class AnnouncementManagerTest
+{
+    // TODO test the actual class, not single methods
+    @Test
+    public void testParseDelay()
+    {
+        AnnouncementManager testamanager = new AnnouncementManager(new Shout());
+
+        assertEquals(1000l, testamanager.parseDelay("1 secound"));
+        assertEquals(60000l, testamanager.parseDelay("1 minute"));
+        assertEquals(3600000l, testamanager.parseDelay("1 hour"));
+        assertEquals(86400000l, testamanager.parseDelay("1 day"));
+
+        assertEquals(172800000l, testamanager.parseDelay("2 days"));
+        assertEquals(2000l, testamanager.parseDelay("2 secounds"));
+        assertEquals(120000l, testamanager.parseDelay("2 minutes"));
+        assertEquals(7200000l, testamanager.parseDelay("2 hours"));
+    }
 }
