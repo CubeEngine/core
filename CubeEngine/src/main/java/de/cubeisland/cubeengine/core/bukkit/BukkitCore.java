af79792eea592617b515440a7180037e4b8080e6
Phillip Schichtel <quick_wango@code-infection.de>
2012-11-09 23:55:09 +0100

    reworked the packet hack to trigger a packet event

--- CubeEngine/src/main/java/de/cubeisland/cubeengine/core/bukkit/BukkitCore.java
+++ CubeEngine/src/main/java/de/cubeisland/cubeengine/core/bukkit/BukkitCore.java
@@ -117,7 +117,7 @@ public class BukkitCore extends JavaPlugin implements Core
         // register Listerer for UserManger
         pm.registerEvents(this.userManager, this);
         
-        pm.registerEvents(new CoreListener(), this);
+        pm.registerEvents(new CoreListener(this), this);
 
         // depends on: file manager, core config
         this.i18n = new I18n(this);
