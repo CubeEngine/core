6d3339c81c6ea26f36e5104eb1cd0d4db0c6e364
Phillip Schichtel <quick_wango@code-infection.de>
2012-11-16 17:18:28 +0100

    fixed and improved the LogLevel

--- CubeEngine/src/main/java/de/cubeisland/cubeengine/core/util/log/LogLevel.java
+++ CubeEngine/src/main/java/de/cubeisland/cubeengine/core/util/log/LogLevel.java
@@ -1,63 +1,39 @@
 package de.cubeisland.cubeengine.core.util.log;
 
+import java.util.Locale;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.logging.Level;
-
-import static de.cubeisland.cubeengine.core.i18n.I18n._;
 
 /**
  * ALL > DEBUG > INFO > NOTICE > WARNING > ERROR > OFF
  */
 public class LogLevel extends Level
 {
+    private static final ConcurrentMap<String, LogLevel> levels = new ConcurrentHashMap<String, LogLevel>(7);
+    
     public static final LogLevel ALL     = new LogLevel("ALL", Integer.MIN_VALUE);
     public static final LogLevel OFF     = new LogLevel("OFF", Integer.MAX_VALUE);
-    public static final LogLevel ERROR   = new LogLevel("ERROR", 1000);
-    public static final LogLevel WARNING = new LogLevel("WARNING", 900);
-    public static final LogLevel NOTICE  = new LogLevel("NOTICE", 800);
-    public static final LogLevel INFO    = new LogLevel("INFO", 700);
-    public static final LogLevel DEBUG   = new LogLevel("DEBUG", 600);
+    public static final LogLevel ERROR   = new LogLevel("Error", 1000);
+    public static final LogLevel WARNING = new LogLevel("Warning", 900);
+    public static final LogLevel NOTICE  = new LogLevel("Notice", 800);
+    public static final LogLevel INFO    = new LogLevel("Info", 700);
+    public static final LogLevel DEBUG   = new LogLevel("Debug", 600);
 
     private LogLevel(String name, int level)
     {
         super(name, level);
+        levels.put(this.getName().toUpperCase(Locale.ENGLISH), this);
     }
 
     @Override
     public String getLocalizedName()
     {
-        return _("core", this.getName());
+        return this.getName();
     }
 
     public static LogLevel parse(String name)
     {
-        if (name.equalsIgnoreCase("ALL"))
-        {
-            return LogLevel.ALL;
-        }
-        if (name.equalsIgnoreCase("OFF"))
-        {
-            return LogLevel.OFF;
-        }
-        if (name.equalsIgnoreCase("ERROR"))
-        {
-            return LogLevel.ERROR;
-        }
-        if (name.equalsIgnoreCase("WARNING"))
-        {
-            return LogLevel.WARNING;
-        }
-        if (name.equalsIgnoreCase("NOTICE"))
-        {
-            return LogLevel.NOTICE;
-        }
-        if (name.equalsIgnoreCase("INFO"))
-        {
-            return LogLevel.INFO;
-        }
-        if (name.equalsIgnoreCase("DEBUG"))
-        {
-            return LogLevel.DEBUG;
-        }
-        return null;
+        return levels.get(name.toUpperCase(Locale.ENGLISH));
     }
 }
