d9c8b6c970cb66f3e73a99dff3c7e398b1847ac9
Anselm Brehme <Faithcaio@yahoo.de>
2012-11-22 13:46:44 +0100

    started #129

---
+++ CubeEngine/src/main/java/de/cubeisland/cubeengine/core/bukkit/BlockUtil.java
@@ -0,0 +1,10 @@
+package de.cubeisland.cubeengine.core.bukkit;
+
+import java.util.EnumSet;
+import org.bukkit.Material;
+
+
+public class BlockUtil
+{
+    public static EnumSet<Material> NON_SOLID_BLOCKS = EnumSet.of(Material.AIR); //TODO add all transparent blocks
+}
