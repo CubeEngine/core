8e1e683d20d8b7ad2773386b40a2c21db818f3cb
Stefan Wolf <boeserwolf91@googlemail.com>
2012-11-23 11:09:49 +0100

    added a little configoptions

--- CubeFun/src/main/java/de/cubeisland/cubeengine/fun/FunConfiguration.java
+++ CubeFun/src/main/java/de/cubeisland/cubeengine/fun/FunConfiguration.java
@@ -12,24 +12,41 @@ public class FunConfiguration extends Configuration
     @Option("lightning.distance")
     public int lightningDistance = 200;
     
+    @Comment("Sets the maximum distance of the explosion")
+    @Option("explosion.distance")
+    public int explosionDistance = 30;
+    @Comment("Sets the maximum power of the explosion")
+    @Option("explosion.power")
+    public int explosionPower = 20;
+    
     @Comment("Sets the maximum number of thrown Objects")
     @Option("throw.number")
-    public int maxThrowNumber = 20;
+    public int maxThrowNumber = 50;
+    @Comment("Sets the maximum delay of this command")
+    @Option("throw.delay")
+    public int maxThrowDelay = 30;
     @Comment("Sets the maximum number of fireballs")
     @Option("fireball.number")
     public int maxFireballNumber = 10;
+    @Comment("Sets the maximum delay of this command")
+    @Option("fireball.delay")
+    public int maxFireballDelay = 30;
+    
     
     @Comment("Sets the maximum amount of changes of day to night and vice versa.")
     @Option("disco.changes")
-    public int maxDiscoChanges = 10;
+    public int maxDiscoChanges = 20;
+    
     
     @Comment("Sets the maximum distance between the mob and the player")
     @Option("invasion.distance")
     public int maxInvasionSpawnDistance = 10;
     
+    
     @Comment("Sets the maximum height a player can jump. Maximum is 100")
     @Option("rocket.height")
     public int maxRocketHeight = 100;
+    
     
     @Comment("Set the maximum distance of the tnt carpet")
     @Option("nuke.distance")
@@ -37,18 +54,10 @@ public class FunConfiguration extends Configuration
     @Comment("Set the nuke radius limit")
     @Option("nuke.radius_limit")
     public int nukeRadiusLimit = 10;
-
-    /*
-     * @Option("nuke.concentrationradius")
-     * public Map<Integer, Integer> nukeRadius = new HashMap<Integer, Integer>()
-     * {
-     * {
-     * put(1, 10);
-     * put(2, 15);
-     * put(3, 20);
-     * put(4, 23);
-     * put(5, 25);
-     * }
-     * };
-     */
+    @Comment("Set the nuke concentration limit")
+    @Option("nuke.concentration_limit") 
+    public int nukeConcentrationLimit = 10;
+    @Comment("Set the maximum range of the explosion")
+    @Option("nuke.explosion_range")
+    public int nukeMaxExplosionRange = 10;
 }
