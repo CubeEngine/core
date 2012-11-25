6ee8c15241d0c002485059a457e0bb8775ad1532
Anselm Brehme <Faithcaio@yahoo.de>
2012-11-25 01:29:44 +0100

    removed old files

--- CubeShout/src/main/java/de/cubeisland/cubeengine/shout/announce/MessageOfTheDay.java
+++
@@ -1,9 +0,0 @@
-package de.cubeisland.cubeengine.shout.announce;
-
-public class MessageOfTheDay extends Announcement
-{
-    public MessageOfTheDay(Announcement announcement)
-    {
-        super(announcement);
-    }
-}
