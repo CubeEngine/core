f6581d6b1342fd370f2d8b197dd605c801e26670
Phillip Schichtel <quick_wango@code-infection.de>
2012-11-25 04:45:55 +0100

    Added the basic files for the roles module

---
+++ Roles/src/main/java/de/cubeisland/cubeengine/roles/Roles.java
@@ -0,0 +1,13 @@
+package de.cubeisland.cubeengine.roles;
+
+import de.cubeisland.cubeengine.core.module.Module;
+
+public class Roles extends Module
+{
+    private RolesConfig config;
+
+    @Override
+    public void onEnable()
+    {
+    }
+}
