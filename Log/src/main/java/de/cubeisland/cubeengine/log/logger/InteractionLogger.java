6951b5254eace5ea4e37064d6072be4ec012188d
Anselm Brehme <Faithcaio@yahoo.de>
2012-11-26 11:09:57 +0100

    [Log] added data for interaction logger and added configuration for it closes #144

--- Log/src/main/java/de/cubeisland/cubeengine/log/logger/InteractionLogger.java
+++ Log/src/main/java/de/cubeisland/cubeengine/log/logger/InteractionLogger.java
@@ -1,16 +1,27 @@
 package de.cubeisland.cubeengine.log.logger;
 
+import de.cubeisland.cubeengine.core.config.annotations.Option;
 import de.cubeisland.cubeengine.core.user.User;
 import de.cubeisland.cubeengine.log.LogAction;
 import de.cubeisland.cubeengine.log.Logger;
 import de.cubeisland.cubeengine.log.SubLogConfig;
+import org.bukkit.block.Block;
+import org.bukkit.block.NoteBlock;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.material.Cake;
+import org.bukkit.material.Diode;
+import org.bukkit.material.Door;
+import org.bukkit.material.Gate;
+import org.bukkit.material.Lever;
+import org.bukkit.material.Openable;
+import org.bukkit.material.TrapDoor;
 
 public class InteractionLogger extends Logger<InteractionLogger.InteractionConfig>
 {
+
     public InteractionLogger()
     {
         super(LogAction.INTERACTION);
@@ -20,30 +31,95 @@ public class InteractionLogger extends Logger<InteractionLogger.InteractionConfi
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onPlayerInteract(PlayerInteractEvent event)
     {
+        Block block = event.getClickedBlock();
         if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK))
         {
             User user = this.module.getUserManager().getExactUser(event.getPlayer());
             switch (event.getClickedBlock().getType())
             {
-                //TODO
-                case WOODEN_DOOR: //open close
-                case TRAP_DOOR://open close
-                case FENCE_GATE://open close
-                case LEVER://on off
+                case WOODEN_DOOR:
+                case TRAP_DOOR:
+                case FENCE_GATE:
+                    if ((block instanceof Door && this.config.logDoor)
+                            || (block instanceof TrapDoor && this.config.logTrapDoor)
+                            || (block instanceof Gate && this.config.logfenceGate))
+                    {
+                        this.module.getLogManager().logInteractLog(user.key, user.getLocation(),
+                                block.getType(), ((Openable) block).isOpen() ? 1 : 0);
+                    }
+                    return;
+                case LEVER:
+                    if (this.config.logLever)
+                    {
+                        this.module.getLogManager().logInteractLog(user.key, user.getLocation(),
+                                block.getType(), ((Lever) block).isPowered() ? 1 : 0);
+                    }
+                    return;
                 case STONE_BUTTON:
                 case WOOD_BUTTON:
-                case CAKE_BLOCK://eat newData
-                case NOTE_BLOCK://changed newData
-                case DIODE_BLOCK_OFF://changed newData
-                case DIODE_BLOCK_ON://changed newData
-                    this.module.getLogManager().logInteractLog(user.key, user.getLocation(), event.getClickedBlock().getType(), null);//TODO
+                    if (this.config.logButtons)
+                    {
+                        this.module.getLogManager().logInteractLog(user.key, user.getLocation(),
+                                block.getType(), null);
+                    }
+                    return;
+                case CAKE_BLOCK: // data: remaining slices
+                    if (this.config.logCake)
+                    {
+                        this.module.getLogManager().logInteractLog(user.key, user.getLocation(),
+                                block.getType(), ((Cake) block).getSlicesRemaining());
+                    }
+                    return;
+                case NOTE_BLOCK:
+                    if (this.config.logNoteBlock)
+                    {
+                        this.module.getLogManager().logInteractLog(user.key, user.getLocation(),
+                                block.getType(), (int) ((NoteBlock) block).getRawNote());
+                    }
+                    return;
+                case DIODE_BLOCK_OFF:
+                case DIODE_BLOCK_ON:
+                    if (this.config.logDiode)
+                    {
+                        this.module.getLogManager().logInteractLog(user.key, user.getLocation(),
+                                block.getType(), ((Diode) block).getDelay());
+                    }
             }
         }
-        //TODO open door via pressure plate ?
+        else if (event.getAction().equals(Action.PHYSICAL) && this.config.logPressurePlate)
+        {
+            User user = this.module.getUserManager().getExactUser(event.getPlayer());
+            switch (block.getType())
+            {
+                case WOOD_PLATE:
+                case STONE_PLATE:
+                    this.module.getLogManager().logInteractLog(user.key, user.getLocation(),
+                            block.getType(), null);
+            }
+        }
     }
 
     public static class InteractionConfig extends SubLogConfig
     {
+        @Option("log-pressureplate")
+        public boolean logPressurePlate = false;
+        @Option("log-door")
+        public boolean logDoor = false;
+        @Option("log-trapDoor")
+        public boolean logTrapDoor = false;
+        @Option("log-fenceGate")
+        public boolean logfenceGate = false;
+        @Option("log-lever")
+        public boolean logLever = false;
+        @Option("log-button")
+        public boolean logButtons = false;
+        @Option("log-cake")
+        public boolean logCake = false;
+        @Option("log-noteblock")
+        public boolean logNoteBlock = false;
+        @Option("log-diode")
+        public boolean logDiode = false;
+
         @Override
         public String getName()
         {
