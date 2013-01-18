d6334600f8c319d5886ff62a255eec686440780b
Anselm Brehme <Faithcaio@yahoo.de>
2013-01-18 23:27:25 +0100

    messing around with the lookup cmd

--- Log/src/main/java/de/cubeisland/cubeengine/log/commands/LogCommands.java
+++ Log/src/main/java/de/cubeisland/cubeengine/log/commands/LogCommands.java
@@ -7,16 +7,16 @@ import de.cubeisland.cubeengine.core.command.annotation.Flag;
 import de.cubeisland.cubeengine.core.command.annotation.Param;
 import de.cubeisland.cubeengine.core.module.Module;
 import de.cubeisland.cubeengine.core.user.User;
+import java.util.Date;
+import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.inventory.ItemStack;
-
-import java.util.Date;
 
 public class LogCommands extends ContainerCommand
 {
     public LogCommands(Module module)
     {
-        super(module, "log", "Searches in the database for needed informations.", "cl");
+        super(module, "lookup", "Searches in the database for needed informations.");
     }
 
     @Command(desc = "Displays all possible parameters.")
@@ -25,9 +25,62 @@ public class LogCommands extends ContainerCommand
         context.sendMessage("You used the /log params command!");
     }
 
-    @Command(names = {
+    /**
+     * Returns the Selection or null if nothing is selected
+     *
+     * @param context
+     * @return
+     */
+    //TODO change return to a selection
+    private Location getSelection(CommandContext context)
+    {
+        if (!context.hasFlag("sel"))
+        {
+            throw new IllegalStateException("Did not choose selection!");
+        }
+        return null;
+        //TODO
+    }
+
+    @Command(names =
+    {
+        "block", "blocklog"
+    }, desc = "Changes regarding blocks",
+             usage = "",
+             flags =
+    {
+        @Flag(longName = "selection", name = "sel"),
+        @Flag(longName = "coordinates", name = "coords"),
+        @Flag(longName = "detailed", name = "det")
+    })
+    // /lookup block [types][radius <r>][block <blocks...>][player <player...>][in world][-selection][-coords][-detailed]
+    //FLAGS:
+    //Selection
+    //show coords
+    //detailed or simple/brief (summing up results or not?)
+    //TYPEs:    
+    //break/place/interact/signs (default: break,place,interact)
+    //WORLD:
+    //in|world world (def current world)
+    //PLAYER
+    //player player1,player2,!notplayer3
+    //including commaseparated exclude with ! infront
+    //BLOCKTYPE
+    //block 35,!1,!wood
+    //same as player matching ,-separated ! infront excludes
+    //RADIUS
+    //radius|area 6
+    //time (since before (timeframe))//<-- TODO very difficult
+    public void block(CommandContext context)
+    {
+    }
+
+    //TODO remove that
+    @Command(names =
+    {
         "lookup"
-    }, desc = "Lookups", usage = "<params>", flags = {
+    }, desc = "Lookups", usage = "<params>", flags =
+    {
         @Flag(longName = "selection", name = "sel"), // only search in Selection
         @Flag(longName = "created", name = "c"), // only search for placed blocks (on by default)
         @Flag(longName = "destroyed", name = "d"), // only search for breaked blocks (on by default)
@@ -35,13 +88,16 @@ public class LogCommands extends ContainerCommand
         @Flag(longName = "chestaccess", name = "chest"), //only search for chestaccess (off by default)
         @Flag(longName = "coordinates", name = "coords"),//display position (off by default)
         @Flag(longName = "descending", name = "desc"), //sort in descending order (default ascending)
-    }, params = {
-        @Param(names = {
+    }, params =
+    {
+        @Param(names =
+        {
             "player", "p"
         }, type = User[].class),
         @Param(names = "area", type = Integer.class),
         @Param(names = "block", type = ItemStack[].class),
-        @Param(names = {
+        @Param(names =
+        {
             "since", "time"
         }, type = Date.class),
         @Param(names = "before", type = Date.class),
