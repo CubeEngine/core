602ebd439c8756af6c93d050f13e3b837792459d
Phillip Schichtel <quick_wango@code-infection.de>
2013-02-09 23:54:07 +0100

    added a misc class for random helper methods

---
+++ CubeEngine/src/main/java/de/cubeisland/cubeengine/core/util/Misc.java
@@ -0,0 +1,12 @@
+package de.cubeisland.cubeengine.core.util;
+
+public final class Misc
+{
+    private Misc()
+    {}
+
+    public static Object[] arr(Object... objects)
+    {
+        return objects;
+    }
+}
