6df821ae7e293e1ab7894b794927fd6f58fe99c1
Phillip Schichtel <quick_wango@code-infection.de>
2013-02-15 00:16:36 +0100

    make our ConsoleCommandSender implement Bukkit's ConsoleCommandSender

--- CubeEngine/src/main/java/de/cubeisland/cubeengine/core/command/sender/ConsoleCommandSender.java
+++ CubeEngine/src/main/java/de/cubeisland/cubeengine/core/command/sender/ConsoleCommandSender.java
@@ -1,9 +1,55 @@
 package de.cubeisland.cubeengine.core.command.sender;
 
-public class ConsoleCommandSender extends WrappedCommandSender
+import org.bukkit.conversations.Conversation;
+import org.bukkit.conversations.ConversationAbandonedEvent;
+
+public class ConsoleCommandSender extends WrappedCommandSender implements org.bukkit.command.ConsoleCommandSender
 {
     public ConsoleCommandSender(org.bukkit.command.ConsoleCommandSender sender)
     {
         super(sender);
+    }
+
+    @Override
+    public org.bukkit.command.ConsoleCommandSender getWrappedSender()
+    {
+        return (org.bukkit.command.ConsoleCommandSender)super.getWrappedSender();
+    }
+
+    @Override
+    public boolean isConversing()
+    {
+
+        return this.getWrappedSender().isConversing();
+    }
+
+    @Override
+    public void acceptConversationInput(String input)
+    {
+        this.getWrappedSender().acceptConversationInput(input);
+    }
+
+    @Override
+    public boolean beginConversation(Conversation conversation)
+    {
+        return this.getWrappedSender().beginConversation(conversation);
+    }
+
+    @Override
+    public void abandonConversation(Conversation conversation)
+    {
+        this.getWrappedSender().abandonConversation(conversation);
+    }
+
+    @Override
+    public void abandonConversation(Conversation conversation, ConversationAbandonedEvent details)
+    {
+        this.getWrappedSender().abandonConversation(conversation, details);
+    }
+
+    @Override
+    public void sendRawMessage(String message)
+    {
+        this.getWrappedSender().sendRawMessage(message);
     }
 }
