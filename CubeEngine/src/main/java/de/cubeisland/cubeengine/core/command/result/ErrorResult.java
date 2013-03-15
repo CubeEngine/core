e03345cf7863debf88ed194d91f81b9e157082b0
totokaka <ttotokaka@gmail.com>
2013-03-15 21:08:25 +0100

    Included the trace in ErrorResult

--- CubeEngine/src/main/java/de/cubeisland/cubeengine/core/command/result/ErrorResult.java
+++ CubeEngine/src/main/java/de/cubeisland/cubeengine/core/command/result/ErrorResult.java
@@ -1,7 +1,9 @@
 package de.cubeisland.cubeengine.core.command.result;
 
+import de.cubeisland.cubeengine.core.CubeEngine;
 import de.cubeisland.cubeengine.core.command.CommandContext;
 import de.cubeisland.cubeengine.core.command.CommandResult;
+import de.cubeisland.cubeengine.core.logger.LogLevel;
 
 public class ErrorResult implements CommandResult
 {
@@ -16,5 +18,6 @@ public class ErrorResult implements CommandResult
     public void show(CommandContext context)
     {
         context.sendMessage("core", "&cAn error occurred while running this command!");
+        CubeEngine.getLogger().log(LogLevel.DEBUG, "Stack trace: ", this.exception);
     }
 }
