b27a8aa84f65cb254fa68b31efd365750fb79a15
Phillip Schichtel <quick_wango@code-infection.de>
2013-04-03 01:41:08 +0200

    CE-336 added the LongRunningResult for commands that have to heavy work on the main thread

---
+++ cubeengine/src/main/java/de/cubeisland/cubeengine/core/command/result/LongRunningResult.java
@@ -0,0 +1,34 @@
+package de.cubeisland.cubeengine.core.command.result;
+
+import de.cubeisland.cubeengine.core.command.CommandContext;
+import de.cubeisland.cubeengine.core.command.CommandResult;
+
+public abstract class LongRunningResult implements CommandResult
+{
+    private boolean isDone = false;
+    private int taskId = -1;
+
+    @Override
+    public void show(final CommandContext context)
+    {
+        this.taskId = context.getCore().getTaskManager().scheduleSyncRepeatingTask(context.getCommand().getModule(), new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                LongRunningResult.this.run(context);
+                if (LongRunningResult.this.isDone)
+                {
+                    context.getCore().getTaskManager().cancelTask(context.getCommand().getModule(), LongRunningResult.this.taskId);
+                }
+            }
+        }, 0, 1);
+    }
+
+    protected void setDone()
+    {
+        this.isDone = true;
+    }
+
+    public abstract void run(CommandContext context);
+}
