4fc4406874e0b6151ffc6b1d2ea03c215bf3eea9
totokaka <ttotokaka@gmail.com>
2013-05-14 21:48:52 +0200

    Very basic outline of the Pagination

---
+++ cubeengine/src/main/java/de/cubeisland/cubeengine/core/command/result/paginated/PaginationManager.java
@@ -0,0 +1,12 @@
+package de.cubeisland.cubeengine.core.command.result.paginated;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tobias
+ * Date: 14.05.13
+ * Time: 21:46
+ * To change this template use File | Settings | File Templates.
+ */
+public class PaginationManager
+{
+}
