bf89838ca4d80867caa5d624c1e527f333ef0316
Anselm Brehme <Faithcaio@yahoo.de>
2013-09-07 19:04:49 +0200

    assign roles sooner

--- roles/src/main/java/de/cubeisland/engine/roles/role/RolesEventHandler.java
+++ roles/src/main/java/de/cubeisland/engine/roles/role/RolesEventHandler.java
@@ -70,7 +70,7 @@ public class RolesEventHandler implements Listener
         }
     }
 
-    @EventHandler(priority = EventPriority.MONITOR)
+    @EventHandler(priority = EventPriority.LOW)
     public void onLogin(PlayerLoginEvent event)
     {
         if (event.getResult().equals(Result.ALLOWED)) // only if allowed to join
