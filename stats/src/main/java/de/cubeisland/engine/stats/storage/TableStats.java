50b4a0f765369f84fc41f44f4ceb09b04403cba4
totokaka <ttotokaka@gmail.com>
2013-11-30 21:20:46 +0100

    License headers and SQL fixing

--- stats/src/main/java/de/cubeisland/engine/stats/storage/TableStats.java
+++ stats/src/main/java/de/cubeisland/engine/stats/storage/TableStats.java
@@ -1,3 +1,20 @@
+/**
+ * This file is part of CubeEngine.
+ * CubeEngine is licensed under the GNU General Public License Version 3.
+ *
+ * CubeEngine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CubeEngine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CubeEngine.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.cubeisland.engine.stats.storage;
 
 import java.sql.Connection;
@@ -29,7 +46,7 @@ public class TableStats extends TableImpl<StatsModel> implements TableCreator<St
     public final Identity<StatsModel, UInteger> IDENTITY;
     public final UniqueKey<StatsModel> PRIMARY_KEY;
 
-    public final TableField<StatsModel, UInteger> KEY = createField("key", SQLDataType.INTEGERUNSIGNED, this);
+    public final TableField<StatsModel, UInteger> KEY = createField("key", SQLDataType.INTEGERUNSIGNED.length(10), this);
     public final TableField<StatsModel, String> STAT = createField("stat", SQLDataType.VARCHAR.length(20), this);
 
     public static TableStats initTable(Database database)
@@ -44,8 +61,8 @@ public class TableStats extends TableImpl<StatsModel> implements TableCreator<St
     {
         connection.prepareStatement("CREATE TABLE IF NOT EXISTS " + this.getName()+ " (\n" +
                                         "`key` int(10) unsigned NOT NULL AUTO_INCREMENT,\n" +
-                                        "`stat` varchar(20) DEFAULT NULL,\n" +
-                                        "PRIMARY KEY (`key`),\n" +
+                                        "`stat` varchar(20) NOT NULL,\n" +
+                                        "PRIMARY KEY (`key`))\n" +
                                         "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci\n" +
                                         "COMMENT='1.0.0'").execute();
     }
