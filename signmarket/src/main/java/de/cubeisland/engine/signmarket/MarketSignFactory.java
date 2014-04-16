d1bf3d91409940cb9d22a19cebb630e8944a5bd9
Anselm Brehme <faithcaio@cubeisland.de>
2014-04-16 23:08:27 +0200

    fixed Admin Marketsigns syncing without checking the item

--- signmarket/src/main/java/de/cubeisland/engine/signmarket/MarketSignFactory.java
+++ signmarket/src/main/java/de/cubeisland/engine/signmarket/MarketSignFactory.java
@@ -156,7 +156,7 @@ public class MarketSignFactory
                 {
                     continue;
                 }
-                if ((sign.isAdminSign() && other.isAdminSign()) || (!sign.isAdminSign() && sign.getRawOwner().equals(other.getRawOwner()) && sign != other)  // same owner (but not same sign)
+                if (((sign.isAdminSign() && other.isAdminSign()) || (!sign.isAdminSign() && sign.getRawOwner().equals(other.getRawOwner()) && sign != other))  // same owner (but not same sign)
                     && sign.canSync(other)) // both have stock AND same item -> doSync
                 {
                     // apply the found item-info to the marketsign
