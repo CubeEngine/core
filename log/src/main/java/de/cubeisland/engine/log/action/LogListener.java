7825d3105f84907c860bd70efdd5a14457e9d7af
Anselm Brehme <faithcaio@cubeisland.de>
2014-05-04 18:38:01 +0200

    fixed disable logging for entire worlds

--- log/src/main/java/de/cubeisland/engine/log/action/LogListener.java
+++ log/src/main/java/de/cubeisland/engine/log/action/LogListener.java
@@ -112,7 +112,7 @@ public class LogListener implements Listener
     public final boolean isActive(Class<? extends BaseAction> clazz, World world)
     {
         LoggingConfiguration config = this.getConfig(world);
-        return this.module.getActionManager().isActive(clazz, config);
+        return config.enableLogging && this.module.getActionManager().isActive(clazz, config);
     }
 
     public <T extends BaseAction> Reference<T> reference(BaseAction action)
