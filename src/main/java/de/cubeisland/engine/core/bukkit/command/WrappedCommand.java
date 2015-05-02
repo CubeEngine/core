3efcb39c909ccaad661f19e90bd2e2e8912ca6ed
Anselm Brehme <faithcaio@cubeisland.de>
2015-05-02 13:42:39 +0200

    log after timing

--- src/main/java/de/cubeisland/engine/core/bukkit/command/WrappedCommand.java
+++ src/main/java/de/cubeisland/engine/core/bukkit/command/WrappedCommand.java
@@ -171,12 +171,12 @@ public class WrappedCommand extends Command
             long delta = System.currentTimeMillis();
             CommandSource source = wrapSender(getModule().getCore(), sender);
             boolean ran = this.command.execute(newInvocation(source, label, args));
-            core.getCommandManager().logExecution(source, ran, this.command, args);
             delta = System.currentTimeMillis() - delta;
             if (delta > 1000 / 20 / 3) // third of a tick
             {
                 core.getLog().warn("The following command used more than third a tick:\n   {} {} | {}ms ({}%)", label, String.join(" ", args), delta, delta * 100 / (1000 / 20) );
             }
+            core.getCommandManager().logExecution(source, ran, this.command, args);
             return ran;
         }
         catch (Exception e)
