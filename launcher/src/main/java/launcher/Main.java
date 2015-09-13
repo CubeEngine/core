dbcbb8c3cf4dcef705c07924ada96da25db475a4
Tim Adamek <tim.adamek@web.de>
2015-09-13 18:37:13 +0200

    Created new modul for launcher

--- launcher/src/main/java/launcher/Main.java
+++ launcher/src/main/java/launcher/Main.java
@@ -1,4 +1,6 @@
-package launcher;import javafx.application.Application;
+package launcher;
+
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -8,31 +10,32 @@ import javafx.stage.Stage;
 
 import java.util.ArrayList;
 
-public class Main extends Application {
+public class Main extends Application
+{
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/launcher/launcher.fxml"));
+    public void start(Stage primaryStage) throws Exception
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("/launcher.fxml"));
         primaryStage.setTitle("Launcher");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.setHeight(400);
         primaryStage.setWidth(600);
         primaryStage.setResizable(false);
         primaryStage.show();
-        VBox checkBoxPane = (VBox) primaryStage.getScene().lookup("#checkBoxPane");
-        ArrayList<CheckBox> checkBoxes= new ArrayList();
-        for (int i=0; i<100;i++){
+        VBox checkBoxPane = (VBox)primaryStage.getScene().lookup("#checkBoxPane");
+        ArrayList<CheckBox> checkBoxes = new ArrayList();
+        for (int i = 0; i < 100; i++)
+        {
             CheckBox cb = new CheckBox(Integer.toString(i));
             checkBoxes.add(cb);
         }
         checkBoxPane.getChildren().addAll(checkBoxes);
-
-
-
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch(args);
     }
 }
