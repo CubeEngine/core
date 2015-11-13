f9da9440c0b485b8149511b7213a51d650f2191b
Anselm Brehme <faithcaio@cubeisland.de>
2015-11-14 00:41:27 +0100

    update for Sponge

--- coremodule/src/main/java/org/cubeengine/module/core/util/matcher/MaterialMatcher.java
+++ coremodule/src/main/java/org/cubeengine/module/core/util/matcher/MaterialMatcher.java
@@ -15,23 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with CubeEngine.  If not, see <http://www.gnu.org/licenses/>.
  */
-/**
- * This file is part of CubeEngine.
- * CubeEngine is licensed under the GNU General Public License Version 3.
- * <p/>
- * CubeEngine is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * <p/>
- * CubeEngine is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * <p/>
- * You should have received a copy of the GNU General Public License
- * along with CubeEngine.  If not, see <http://www.gnu.org/licenses/>.
- */
 package org.cubeengine.module.core.util.matcher;
 
 import java.util.Arrays;
@@ -104,10 +87,10 @@ public class MaterialMatcher
     public MaterialMatcher( Game game)
     {
         this.game = game;
-        this.builder = game.getRegistry().createItemBuilder();
+        this.builder = game.getRegistry().createBuilder(ItemStackBuilder.class);
 
         // Read names from GameDirectory
-        for (Entry<String, Set<ItemType>> entry : game.getRegistry().getGameDictionary().getAllItems().entrySet())
+        for (Entry<String, Set<ItemType>> entry : game.getGameDictionary().getAllItems().entrySet())
         {
             names.put(entry.getKey(), entry.getValue().iterator().next());
         }
