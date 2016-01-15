24db4843dda4333228d82d8f0e1d449a327eb9ed
Anselm Brehme <faithcaio@cubeisland.de>
2016-01-15 23:33:01 +0100

    implement getBlockBehindWall

--- coremodule/src/main/java/org/cubeengine/module/core/util/LocationUtil.java
+++ coremodule/src/main/java/org/cubeengine/module/core/util/LocationUtil.java
@@ -17,18 +17,24 @@
  */
 package org.cubeengine.module.core.util;
 
+import com.flowpowered.math.imaginary.Quaterniond;
+import com.flowpowered.math.vector.Vector3d;
+import org.spongepowered.api.block.BlockType;
+import org.spongepowered.api.data.property.AbstractProperty;
+import org.spongepowered.api.data.property.block.MatterProperty;
 import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.util.blockray.BlockRay;
+import org.spongepowered.api.util.blockray.BlockRayHit;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 
+import java.util.Iterator;
+import java.util.Optional;
+
+import static org.spongepowered.api.data.property.block.MatterProperty.Matter.SOLID;
+
 public class LocationUtil
 {
-    public static Location<World> getBlockBehindWall(Player user, int maxDistanceToWall, int maxThicknessOfWall)
-    {
-        // TODO
-        return null;
-    }
-
     public static long getChunkKey(Location loc)
     {
         int chunkX = loc.getBlockX() >> 4;
@@ -47,5 +53,43 @@ public class LocationUtil
     public static long getChunkKey(int chunkX, int chunkZ)
     {
         return ((long)chunkX << 32) | chunkZ & 0xFFFFFFFFL;
+    }
+
+    public static Optional<Location<World>> getBlockBehindWall(Player player, int maxRange, int maxWallThickness)
+    {
+        Iterator<BlockRayHit<World>> it = BlockRay.from(player).blockLimit(maxRange).iterator();
+        Optional<BlockRayHit<World>> end = Optional.empty();
+
+        while (it.hasNext())
+        {
+            BlockRayHit<World> hit = it.next();
+            BlockType blockType = hit.getExtent().getBlockType(hit.getBlockX(), hit.getBlockY(), hit.getBlockZ());
+            if (blockType.getProperty(MatterProperty.class).map(AbstractProperty::getValue).orElse(null) == SOLID)
+            {
+                end = Optional.of(hit);
+                break;
+            }
+        }
+        if (!end.isPresent())
+        {
+            return Optional.empty();
+        }
+
+        Vector3d rotation = player.getRotation();
+        rotation = Quaterniond.fromAxesAnglesDeg(rotation.getX(), -rotation.getY(), rotation.getZ()).getDirection();
+        it = BlockRay.from(end.get().getExtent(), end.get().getPosition()).direction(rotation).blockLimit(maxWallThickness).iterator();
+        end = Optional.empty();
+        while (it.hasNext())
+        {
+            BlockRayHit<World> hit = it.next();
+            BlockType blockType = hit.getExtent().getBlockType(hit.getBlockX(), hit.getBlockY(), hit.getBlockZ());
+            if (blockType.getProperty(MatterProperty.class).map(AbstractProperty::getValue).orElse(null) != SOLID)
+            {
+                end = Optional.of(hit);
+                break;
+            }
+        }
+
+        return end.map(BlockRayHit::getLocation);
     }
 }
