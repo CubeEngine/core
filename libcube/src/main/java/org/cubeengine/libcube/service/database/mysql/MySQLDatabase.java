75dc2ba8d81ce0ca810adfe6f0ee2e451df41c4a
Anselm Brehme <faithcaio@cubeisland.de>
2016-05-14 13:31:53 +0200

    set timeout

--- libcube/src/main/java/org/cubeengine/libcube/service/database/mysql/MySQLDatabase.java
+++ libcube/src/main/java/org/cubeengine/libcube/service/database/mysql/MySQLDatabase.java
@@ -15,23 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with CubeEngine.  If not, see <http://www.gnu.org/licenses/>.
  */
-/**
- * This file is part of CubeEngine.
- * CubeEngine is licensed under the GNU General Public License Version 3.
- * <p/>
- * CubeEngine is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * <p/>
- * CubeEngine is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * <p/>
- * You should have received a copy of the GNU General Public License
- * along with CubeEngine.  If not, see <http://www.gnu.org/licenses/>.
- */
 package org.cubeengine.libcube.service.database.mysql;
 
 import java.io.File;
@@ -134,7 +117,7 @@ public class MySQLDatabase extends AbstractDatabase implements Database, Modular
         dsConf.setMaximumPoolSize(20);
         dsConf.setThreadFactory(threadFactory);
         dataSource = new HikariDataSource(dsConf);
-
+        dataSource.setConnectionTimeout(100000); // 10s
         try (Connection connection = dataSource.getConnection())
         {
             try (PreparedStatement s = connection.prepareStatement("SHOW variables WHERE Variable_name='wait_timeout'"))
