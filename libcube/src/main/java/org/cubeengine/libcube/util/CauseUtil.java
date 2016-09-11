1613c04749943665da5b82ff060ade79001cc7e3
Anselm Brehme <faithcaio@cubeisland.de>
2016-09-11 13:22:41 +0200

    CauseUtil

--- libcube/src/main/java/org/cubeengine/libcube/util/CauseUtil.java
+++ libcube/src/main/java/org/cubeengine/libcube/util/CauseUtil.java
@@ -4,6 +4,7 @@ import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.event.cause.Cause;
 import org.spongepowered.api.event.cause.NamedCause;
 import org.spongepowered.api.event.cause.entity.spawn.SpawnCause;
+import org.spongepowered.api.event.cause.entity.spawn.SpawnType;
 import org.spongepowered.api.event.cause.entity.spawn.SpawnTypes;
 
 public class CauseUtil
@@ -12,7 +13,11 @@ public class CauseUtil
 
     public static Cause spawnCause(CommandSource source)
     {
-        return Cause.of(NamedCause.source(SpawnCause.builder().type(SpawnTypes.PLUGIN).build()),
-                        NamedCause.notifier(source));
+        return spawnCause(SpawnTypes.PLUGIN, source);
+    }
+
+    public static Cause spawnCause(SpawnType spawnType, CommandSource source)
+    {
+        return Cause.of(NamedCause.source(SpawnCause.builder().type(spawnType).build()), NamedCause.notifier(source));
     }
 }
