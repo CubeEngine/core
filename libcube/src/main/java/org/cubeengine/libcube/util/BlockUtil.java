090be25f3744ec05bac8bfa5442be2a21f3fd745
Anselm Brehme <faithcaio@cubeisland.de>
2017-03-21 22:41:11 +0100

    LagFreeBlockWorker

--- libcube/src/main/java/org/cubeengine/libcube/util/BlockUtil.java
+++ libcube/src/main/java/org/cubeengine/libcube/util/BlockUtil.java
@@ -17,26 +17,96 @@
  */
 package org.cubeengine.libcube.util;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
+import static org.spongepowered.api.block.BlockTypes.ACACIA_DOOR;
+import static org.spongepowered.api.block.BlockTypes.ACTIVATOR_RAIL;
+import static org.spongepowered.api.block.BlockTypes.AIR;
+import static org.spongepowered.api.block.BlockTypes.BIRCH_DOOR;
+import static org.spongepowered.api.block.BlockTypes.BROWN_MUSHROOM;
+import static org.spongepowered.api.block.BlockTypes.CACTUS;
+import static org.spongepowered.api.block.BlockTypes.CARROTS;
+import static org.spongepowered.api.block.BlockTypes.COCOA;
+import static org.spongepowered.api.block.BlockTypes.DARK_OAK_DOOR;
+import static org.spongepowered.api.block.BlockTypes.DEADBUSH;
+import static org.spongepowered.api.block.BlockTypes.DETECTOR_RAIL;
+import static org.spongepowered.api.block.BlockTypes.FIRE;
+import static org.spongepowered.api.block.BlockTypes.FLOWER_POT;
+import static org.spongepowered.api.block.BlockTypes.FLOWING_LAVA;
+import static org.spongepowered.api.block.BlockTypes.FLOWING_WATER;
+import static org.spongepowered.api.block.BlockTypes.GOLDEN_RAIL;
+import static org.spongepowered.api.block.BlockTypes.GRAVEL;
+import static org.spongepowered.api.block.BlockTypes.HEAVY_WEIGHTED_PRESSURE_PLATE;
+import static org.spongepowered.api.block.BlockTypes.IRON_DOOR;
+import static org.spongepowered.api.block.BlockTypes.JUNGLE_DOOR;
+import static org.spongepowered.api.block.BlockTypes.LAVA;
+import static org.spongepowered.api.block.BlockTypes.LEVER;
+import static org.spongepowered.api.block.BlockTypes.LIGHT_WEIGHTED_PRESSURE_PLATE;
+import static org.spongepowered.api.block.BlockTypes.MELON_STEM;
+import static org.spongepowered.api.block.BlockTypes.NETHER_WART;
+import static org.spongepowered.api.block.BlockTypes.PORTAL;
+import static org.spongepowered.api.block.BlockTypes.POTATOES;
+import static org.spongepowered.api.block.BlockTypes.POWERED_COMPARATOR;
+import static org.spongepowered.api.block.BlockTypes.POWERED_REPEATER;
+import static org.spongepowered.api.block.BlockTypes.PUMPKIN_STEM;
+import static org.spongepowered.api.block.BlockTypes.RAIL;
+import static org.spongepowered.api.block.BlockTypes.REDSTONE_TORCH;
+import static org.spongepowered.api.block.BlockTypes.REDSTONE_WIRE;
+import static org.spongepowered.api.block.BlockTypes.RED_FLOWER;
+import static org.spongepowered.api.block.BlockTypes.RED_MUSHROOM;
+import static org.spongepowered.api.block.BlockTypes.REEDS;
+import static org.spongepowered.api.block.BlockTypes.SAND;
+import static org.spongepowered.api.block.BlockTypes.SAPLING;
+import static org.spongepowered.api.block.BlockTypes.SKULL;
+import static org.spongepowered.api.block.BlockTypes.SNOW;
+import static org.spongepowered.api.block.BlockTypes.SPRUCE_DOOR;
+import static org.spongepowered.api.block.BlockTypes.STANDING_SIGN;
+import static org.spongepowered.api.block.BlockTypes.STONE_PRESSURE_PLATE;
+import static org.spongepowered.api.block.BlockTypes.TALLGRASS;
+import static org.spongepowered.api.block.BlockTypes.TORCH;
+import static org.spongepowered.api.block.BlockTypes.TRIPWIRE;
+import static org.spongepowered.api.block.BlockTypes.TRIPWIRE_HOOK;
+import static org.spongepowered.api.block.BlockTypes.UNLIT_REDSTONE_TORCH;
+import static org.spongepowered.api.block.BlockTypes.UNPOWERED_COMPARATOR;
+import static org.spongepowered.api.block.BlockTypes.UNPOWERED_REPEATER;
+import static org.spongepowered.api.block.BlockTypes.VINE;
+import static org.spongepowered.api.block.BlockTypes.WALL_SIGN;
+import static org.spongepowered.api.block.BlockTypes.WATER;
+import static org.spongepowered.api.block.BlockTypes.WATERLILY;
+import static org.spongepowered.api.block.BlockTypes.WEB;
+import static org.spongepowered.api.block.BlockTypes.WHEAT;
+import static org.spongepowered.api.block.BlockTypes.WOODEN_DOOR;
+import static org.spongepowered.api.block.BlockTypes.WOODEN_PRESSURE_PLATE;
+import static org.spongepowered.api.block.BlockTypes.YELLOW_FLOWER;
+import static org.spongepowered.api.util.Direction.DOWN;
+import static org.spongepowered.api.util.Direction.EAST;
+import static org.spongepowered.api.util.Direction.NORTH;
+import static org.spongepowered.api.util.Direction.SOUTH;
+import static org.spongepowered.api.util.Direction.UP;
+import static org.spongepowered.api.util.Direction.WEST;
+
 import org.spongepowered.api.Game;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.block.BlockType;
+import org.spongepowered.api.block.tileentity.TileEntityArchetype;
 import org.spongepowered.api.data.manipulator.mutable.block.AttachedData;
 import org.spongepowered.api.data.manipulator.mutable.block.PortionData;
 import org.spongepowered.api.data.type.Hinge;
 import org.spongepowered.api.data.type.Hinges;
 import org.spongepowered.api.data.type.PortionTypes;
+import org.spongepowered.api.event.cause.Cause;
+import org.spongepowered.api.plugin.PluginContainer;
 import org.spongepowered.api.util.Direction;
+import org.spongepowered.api.world.BlockChangeFlag;
 import org.spongepowered.api.world.Chunk;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
+import org.spongepowered.api.world.extent.ArchetypeVolume;
 import org.spongepowered.api.world.storage.ChunkLayout;
 
-import static org.spongepowered.api.block.BlockTypes.*;
-import static org.spongepowered.api.util.Direction.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 /**
  * Provides Utils for blocks in Bukkit.
@@ -267,5 +337,77 @@ public class BlockUtil
             direction = direction.getOpposite();
         }
         return direction;
+    }
+
+    public static void applyVolume(ArchetypeVolume volume, Location<World> at, Cause cause, PluginContainer plugin)
+    {
+        new LagFreeBlockWorker(volume, at, cause, plugin).run();
+    }
+
+    private static class LagFreeBlockWorker
+    {
+        private ArchetypeVolume volume;
+        private Location<World> at;
+        private Cause cause;
+        private PluginContainer plugin;
+
+        public LagFreeBlockWorker(ArchetypeVolume volume, Location<World> at, Cause cause, PluginContainer plugin)
+        {
+            this.volume = volume;
+            this.at = at;
+            this.cause = cause;
+            this.plugin = plugin;
+        }
+
+        private final int xMin = volume.getBlockMin().getX();
+        private final int yMin = volume.getBlockMin().getY();
+        private final int zMin = volume.getBlockMin().getZ();
+        private final int xMax = volume.getBlockMax().getX();
+        private final int yMax = volume.getBlockMax().getY();
+        private final int zMax = volume.getBlockMax().getZ();
+
+        private int z = zMin;
+        private int y = yMin;
+        private int x = xMin;
+
+        private static final long MSPT = 50; // 20 TPS = 1T/50MS
+        private int tickPercentage = 60;
+
+        public void run()
+        {
+            if (!processVolume())
+            {
+                Sponge.getScheduler().createTaskBuilder().delayTicks(1).execute(this::run).name("CE-BlockWorker").submit(plugin);
+            }
+        }
+
+        private boolean processVolume()
+        {
+            // TODO entities?
+            long msLimit = MSPT * tickPercentage / 100;
+            long start = System.currentTimeMillis();
+            int i = 0;
+            for (; z <= zMax; z++)
+            {
+                for (; y <= yMax; y++)
+                {
+                    for (; x <= xMax; x++)
+                    {
+                        i++;
+                        at.getExtent().setBlock(at.getBlockX() + x, at.getBlockY() + y, at.getBlockZ() + z, volume.getBlock(x,y,z), BlockChangeFlag.NONE, cause);
+                        Optional<TileEntityArchetype> te = volume.getTileEntityArchetype(x, y, z);
+                        te.ifPresent(tileEntityArchetype -> tileEntityArchetype.apply(at.add(x, y, z), cause));
+                        if (i % 100 == 0)
+                        {
+                            if ((System.currentTimeMillis() - start) > msLimit)
+                            {
+                                return x != xMax || y != yMax || z != zMax;
+                            }
+                        }
+                    }
+                }
+            }
+            return true;
+        }
     }
 }
