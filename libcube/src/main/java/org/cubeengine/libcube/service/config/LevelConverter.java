8c222e447184c602b4620c5d7c5740455d4082f6
Anselm Brehme <faithcaio@cubeisland.de>
2017-05-25 10:55:04 +0200

    headerpocalypse

--- libcube/src/main/java/org/cubeengine/libcube/service/config/LevelConverter.java
+++ libcube/src/main/java/org/cubeengine/libcube/service/config/LevelConverter.java
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with CubeEngine.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.cubeengine.libcube.service.config;
 
 import org.cubeengine.converter.ConversionException;
