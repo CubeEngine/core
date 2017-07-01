42f297705687cc96fe646aabd2ec91b76a78a026
Phillip Schichtel <phillip.public@schich.tel>
2017-07-01 19:43:11 +0200

    remove the redundant documentation fields

--- docs/src/main/java/org/cubeengine/module/docs/Info.java
+++ docs/src/main/java/org/cubeengine/module/docs/Info.java
@@ -23,9 +23,5 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Info extends ReflectedYaml {
-
-    public String version;
-    public String sourceVersion;
-    public String buildTimeStamp;
     public List<String> features = new ArrayList<>();
 }
