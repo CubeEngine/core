b8494333e22735c81a7c0759eb88b53a9b7044f3
Anselm Brehme <faithcaio@cubeisland.de>
2017-10-28 10:13:38 +0200

    add SpawnUtil spawnItem drop item for selectiontool and logtool

---
+++ libcube/src/main/java/org/cubeengine/libcube/util/SpawnUtil.java
@@ -0,0 +1,35 @@
+/*
+ * This file is part of CubeEngine.
+ * CubeEngine is licensed under the GNU General Public License Version 3.
+ *
+ * CubeEngine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CubeEngine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CubeEngine.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.cubeengine.libcube.util;
+
+import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.entity.Entity;
+import org.spongepowered.api.entity.EntityTypes;
+import org.spongepowered.api.item.inventory.ItemStack;
+import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
+
+public class SpawnUtil
+{
+    public static void spawnItem(ItemStack item, Location<World> at)
+    {
+        Entity entity = at.getExtent().createEntityNaturally(EntityTypes.ITEM, at.getPosition());
+        entity.offer(Keys.REPRESENTED_ITEM, item.createSnapshot());
+        at.getExtent().spawnEntity(entity);
+    }
+}
