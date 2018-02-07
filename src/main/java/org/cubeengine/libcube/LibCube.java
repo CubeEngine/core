7298363a359c75452e4e86ac33b7cd06e66eeb10
Anselm Brehme <faithcaio@cubeisland.de>
2018-02-08 00:06:24 +0100

    release

--- src/main/java/org/cubeengine/libcube/LibCube.java
+++ src/main/java/org/cubeengine/libcube/LibCube.java
@@ -30,7 +30,7 @@ import java.nio.file.Path;
 
 import javax.inject.Inject;
 
-@Plugin(id = "cubeengine-core", name = "LibCube", version = "1.2.1-SNAPSHOT",
+@Plugin(id = "cubeengine-core", name = "LibCube", version = "1.2.1",
         description = "Core Library for CubeEngine plugins",
         url = "http://cubeengine.org",
         authors = "CubeEngine Team")
