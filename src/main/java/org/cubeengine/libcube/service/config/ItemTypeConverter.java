fe4e5db3356bcae050838adbd86b152a8563b4ce
Anselm Brehme <development.anselm@breh.me>
2021-04-05 13:02:09 +0200

    better itemtype conversion

--- src/main/java/org/cubeengine/libcube/service/config/ItemTypeConverter.java
+++ src/main/java/org/cubeengine/libcube/service/config/ItemTypeConverter.java
@@ -31,7 +31,7 @@ public class ItemTypeConverter extends SimpleConverter<ItemType>
     @Override
     public Node toNode(ItemType object)
     {
-        return StringNode.of(Sponge.game().registries().registry(RegistryTypes.ITEM_TYPE).valueKey(object).asString());
+        return StringNode.of(RegistryTypes.ITEM_TYPE.get().valueKey(object).asString());
     }
 
     @Override
@@ -39,7 +39,7 @@ public class ItemTypeConverter extends SimpleConverter<ItemType>
     {
         if (node instanceof StringNode)
         {
-            return Sponge.game().registries().registry(RegistryTypes.ITEM_TYPE).findValue(ResourceKey.resolve(node.asText())).orElse(null);
+            return RegistryTypes.ITEM_TYPE.get().findValue(ResourceKey.resolve(node.asText())).orElse(null);
         }
         throw ConversionException.of(this, node, "Node is not a StringNode!");
     }
