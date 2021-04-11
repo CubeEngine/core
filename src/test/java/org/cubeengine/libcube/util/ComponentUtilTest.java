62b789f314f84fc8bd9da1342bc6177ced9a27cf
Phillip Schichtel <phillip@schich.tel>
2021-04-11 18:57:16 +0200

    increase the test coverage

--- src/test/java/org/cubeengine/libcube/util/ComponentUtilTest.java
+++ src/test/java/org/cubeengine/libcube/util/ComponentUtilTest.java
@@ -17,8 +17,10 @@
  */
 package org.cubeengine.libcube.util;
 
+import net.kyori.adventure.text.Component;
 import org.junit.Test;
 
+import static net.kyori.adventure.text.Component.*;
 import static org.junit.Assert.*;
 
 public class ComponentUtilTest
@@ -26,6 +28,12 @@ public class ComponentUtilTest
     @Test
     public void autoLink()
     {
-        assertEquals(ComponentUtil.clickableLink("http://test", "http://test", "open"), ComponentUtil.autoLink("http://test", "open"));
+        final String hover = "open";
+        final Component link = ComponentUtil.clickableLink("http://test", "http://test", hover);
+
+        assertEquals(link, ComponentUtil.autoLink("http://test", hover));
+        assertEquals(empty(), ComponentUtil.autoLink("", hover));
+        assertEquals(join(empty(), space(), link, space()), ComponentUtil.autoLink(" http://test ", hover));
+        assertEquals(join(empty(), space(), link, space(), link, space()), ComponentUtil.autoLink(" http://test http://test ", hover));
     }
 }
